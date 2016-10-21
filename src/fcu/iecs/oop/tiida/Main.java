849f2c3c97ec58b05437c7e746ca7f3107da9e6d
user <user@x450j-nb>
2016-10-21 20:34:32 +0800

    HW4 Exercise2

--- src/fcu/iecs/oop/tiida/Main.java
+++ src/fcu/iecs/oop/tiida/Main.java
@@ -1,10 +1,24 @@
 package fcu.iecs.oop.tiida;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		NissanTiida x = new NissanTiida();
+		Scanner keyboard = new Scanner(System.in);
+		int i;
+		System.out.println("Please enter a number : ");
+		int n = keyboard.nextInt();
+		
+		for(i=0;i<n;i++)
+		{
+			x.tiida();
+			System.out.printf("%n");
+		}
+		
+		keyboard.close();
 	}
 
 }
--- src/fcu/iecs/oop/tiida/NissanTiida.java
+++ src/fcu/iecs/oop/tiida/NissanTiida.java
@@ -1,5 +1,22 @@
 package fcu.iecs.oop.tiida;
 
 public class NissanTiida {
+	
+	public int count = 0;
+	public  void tiida()
+	{
+		count++;
+		int i,j;
+		for(i=0;i<count;i++)
+		{
+			{
+				for(j=0;j<count;j++)
+				{
+					System.out.printf("*");
+				}
+				System.out.printf("%n");
+			}
+		}
 
+	}
 }
